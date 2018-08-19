445946d39d74b436051a64f82200836c3a949d1d
Lee <15537186765@163.com>
2018-08-20 01:48:20 +0800

    配置redis

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 app = Flask(__name__)
 
@@ -13,11 +14,18 @@ class Config(object):
     # 禁用追踪mysql:因为mysql的性能差，如果再去追踪mysql的所有的修改，会再次浪费性能
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # 配置redis
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
 # 加载app的配置
 app.config.from_object(Config)
 
 # 配置MySQL
 db = SQLAlchemy(app)
+
+# 配置redis
+redis_store = StrictRedis(host = Config.REDIS_HOST,port = Config.REDIS_PORT)
 
 @app.route('/')
 def index():
