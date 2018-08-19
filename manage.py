fb197079b7a02566871ea258ee1407399a4acf88
Lee <15537186765@163.com>
2018-08-20 01:41:13 +0800

    准备配置类和开启调试模式

--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
+# 准备配置类
+class Config(object):
+    """app配置类"""
+    DEBUG = True
+# 加载app的配置
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
